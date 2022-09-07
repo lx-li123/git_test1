0f9284a95dfc24a95e007d40468ed48986b5a07d
lixu <1924965153@qq.com>
2022-09-07 20:36:47 +0800

    push test commit2

--- src/main/java/lx/GitTest.java
+++ src/main/java/lx/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello hot-fix");
         System.out.println("hello hotfix1");
+        System.out.println("push test");
     }
 }
